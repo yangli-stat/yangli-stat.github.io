f431c388457d9c4d55eaf14143812abe7e806b30
yangli-stat <58796315+yangli-stat@users.noreply.github.com>
2021-08-16 08:40:49 -0400

    Add files via upload

---
+++ SLR1.R
@@ -0,0 +1,10 @@
+prod <- read.table("http://yangli.us/prod.txt", header=T)
+fit <- lm(RunTime ~ RunSize, data=prod)
+fit$residuals
+
+plot(prod$RunSize, prod$RunTime, xlab="Size", ylab="Time")
+abline(fit)
+
+newdata <- as.data.frame(c(100, 150, 200, 250))
+colnames(newdata) <- "RunSize"
+predict(fit, newdata)
